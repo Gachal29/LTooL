2dee623b35e9497e21cf5d0b6f3a6ac302de77f0
Gachal29 <taisuke0129t@gmail.com>
2022-12-12 22:33:09 +0900

    モデルの作成

--- ltool/user_manager/models.py
+++ ltool/user_manager/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user: models.OneToOneField = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    view_name: models.CharField = models.CharField(max_length=150, verbose_name="表示名")
+    icon: models.ImageField = models.ImageField(upload_to="icons", blank=True, verbose_name="アイコン")
+    introduction: models.CharField = models.CharField(max_length=1024, blank=True, verbose_name="自己紹介")
+
+    def __str__(self) -> str:
+        return f"id:{self.id}, user:{self.user}"
+
+    class Meta:
+        verbose_name_plural: str = "プロフィール"
+
+
+class ScrapboxProjects(models.Model):
+    user: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
+    project: models.CharField = models.CharField(max_length=25, verbose_name="プロジェクト")
+
+    def __str__(self) -> str:
+        return f"id:{self.id} user:{self.user} project:{self.project}"
+
+    class Meta:
+        verbose_name_plural: str = "Scrapboxプロジェクト"
