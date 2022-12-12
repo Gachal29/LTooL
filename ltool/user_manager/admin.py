1f4a6103895873cdbf5f368ec667e7d62927df8a
Gachal29 <taisuke0129t@gmail.com>
2022-12-12 22:36:09 +0900

    モデルを管理に追加、コード整形

--- ltool/user_manager/admin.py
+++ ltool/user_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from ltool.user_manager.models import Profile, ScrapboxProjects
+
+admin.site.register(Profile)
+admin.site.register(ScrapboxProjects)
