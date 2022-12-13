39769e251588a4e47ed70b9f0817fc2e8e1a0952
Gachal29 <taisuke0129t@gmail.com>
2022-12-13 10:05:01 +0900

    base.htmlの変更

--- ltool/views.py
+++ ltool/views.py
@@ -4,3 +4,9 @@ from django.contrib.auth.views import LoginView
 
 class CustomLoginView(LoginView):
     form_class: AuthenticationForm = AuthenticationForm
+
+    def get_context_data(self, **kwargs: any) -> dict[str, any]:
+        context: dict[str, any] = super().get_context_data(**kwargs)
+        context["auth_page"] = True
+
+        return context
